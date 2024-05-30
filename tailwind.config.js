1a3f7a2533944fc93b8a24bb09edba367ae0857f
samrfreitas <samuelrfreitassi@gmail.com>
2024-05-29 23:36:32 -0300

    style: adiciona novas propriedades no tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,27 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#AA69FF',
+        'primary-dark': '#7B3FAF',
+        'secondary': '#00ff00', // Defina a cor secundária como verde
+        'custom-blue': {
+          50: '#e3f2fd',
+          100: '#bbdefb',
+          200: '#90caf9',
+          300: '#64b5f6',
+          400: '#42a5f5',
+          500: '#2196f3',
+          600: '#1e88e5',
+          700: '#1976d2',
+          800: '#1565c0',
+          900: '#0d47a1'
+        },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    // require('@tailwindcss/forms'),
+  ],
+}}
 
