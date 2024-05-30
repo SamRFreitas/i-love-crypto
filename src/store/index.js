91efbbb1df6bc3a9c7784e51e1706da53c87ea0f
samrfreitas <samuelrfreitassi@gmail.com>
2024-05-29 23:39:46 -0300

    feature: add store in the project

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,24 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    usedCoins: ['bitcoin', 'dacxi', 'ethereum', 'cosmos' ],
+    coins: [],
   },
   getters: {
+    getUsedCoins: (state) => {
+      return state.usedCoins
+    },
+    getCoins: (state) => {
+      return state.coins
+    },
+    getCoinById: (state) => (id) => {
+      return state.coins.find(coin => coin.id === id)
+    },
   },
   mutations: {
+    SET_COINS(state, coins) {
+      state.coins = coins
+    }
   },
   actions: {
   },
