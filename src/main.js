0ab042704d733af74f7f8136ea5fe38cde208ba3
samrfreitas <samuelrfreitassi@gmail.com>
2024-05-29 23:37:24 -0300

    feauture: add datepicker component

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/css/index.css'
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).component('VueDatePicker', VueDatePicker).mount('#app')
