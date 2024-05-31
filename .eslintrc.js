3d5f13d7169c80984ca5d8afb32093b064fd5a64
samrfreitas <samuelrfreitassi@gmail.com>
2024-05-30 21:21:51 -0300

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,16 +14,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    'semi': ['error', 'never']
+  }
 }
