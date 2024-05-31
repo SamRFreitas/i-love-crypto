6be250c7444d56989d5e27686ccc3a4c7e546be4
samrfreitas <samuelrfreitassi@gmail.com>
2024-05-30 21:23:11 -0300

    feature: new component to image

---
+++ src/components/CoinImage.vue
@@ -0,0 +1,13 @@
+<template>
+    <img :src="require(`../assets/images/${props.id}.png`)" alt="" class="flex-shrink-0 rounded-full" :class="[width, height]" />
+</template>
+
+<script setup>
+
+    const props = defineProps({
+        id: { type: String, required: true },
+        width: { type: String, default: 'w-5' },
+        height: { type: String, default: 'h-5' },
+    })
+
+</script>
