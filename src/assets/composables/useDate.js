3484bc7a46cff196a68ee6ec061e7968e7056aaf
samrfreitas <samuelrfreitassi@gmail.com>
2024-05-30 21:22:32 -0300

    feature: add composable to use date

---
+++ src/assets/composables/useDate.js
@@ -0,0 +1,45 @@
+export default (datetime) => {
+
+    const formatDateTimeToUNIXTimestamp = () => {
+    
+        console.log(datetime)
+    
+        const date = new Date(datetime)
+
+        const unixTimeSeconds = Math.floor(date.getTime() / 1000)
+
+        return unixTimeSeconds
+    }
+
+    const add1HourTimeIntervalToUnixTimeSeconds = () => {
+
+        return unixTimeSeconds + (60 * 60)
+    }
+
+    const formatDate = (datetimeToFormat) => {
+
+        console.log(datetimeToFormat)
+
+        const date = new Date(datetimeToFormat)
+        const day = date.getDate().toString().padStart(2, '0')
+        const month = (date.getMonth() + 1).toString().padStart(2, '0')
+        const year = date.getFullYear()
+        const hours = date.getHours().toString().padStart(2, '0')
+        const minutes = date.getMinutes().toString().padStart(2, '0')
+
+        const formatedDate = `${month}/${day}/${year}, ${hours}:${minutes}`
+        console.log(formatedDate)
+        return formatedDate
+    }
+    
+
+    const unixTimeSeconds = formatDateTimeToUNIXTimestamp(datetime)
+
+    const range = {
+        from: unixTimeSeconds,
+        to: add1HourTimeIntervalToUnixTimeSeconds(unixTimeSeconds)
+    }
+
+    return { range, formatDate }
+
+}
