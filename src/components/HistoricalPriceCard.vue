4421d876d6a302359dc5518ac2868fb6e89f6b9e
samrfreitas <samuelrfreitassi@gmail.com>
2024-05-30 23:28:40 -0300

    style: chage the max width

--- src/components/HistoricalPriceCard.vue
+++ src/components/HistoricalPriceCard.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="mt-6 max-w-lg flex flex-col h-32 justify-center items-center space-y-3 px-4 md:shadow-xl border rounded-md w-full">
+    <div class="mt-6 max-w-md flex flex-col h-32 justify-center items-center space-y-3 px-4 md:shadow-xl border rounded-md w-full">
         <CoinImage :id="props.historicalData.coin.id" :width="'w-8'" :height="'h-8'" />
 
         <div class="flex flex-row justify-between w-full">
             <label class="block text-md font-medium leading-6 text-gray-900"> {{ props.historicalData.coin.name }} Price: </label>
-            <label class="block text-md font-medium leading-6 text-gray-900"> ${{ props.historicalData.price }} </label>
+            <label class="block text-md font-medium leading-6 text-gray-900"> {{ props.historicalData.price }} </label>
         </div>
 
         <div class="flex flex-row justify-between w-full">
@@ -16,7 +16,7 @@
 
 <script setup>
 import CoinImage from '@/components/CoinImage.vue'
-import useDate from '@/assets/composables/useDate'
+import useDate from '@/composables/useDate'
 
 const props = defineProps({
     historicalData: { type: Object },
