900e5dc2d1b8100fdb42c1f7235d24356aa20233
samrfreitas <samuelrfreitassi@gmail.com>
2024-06-01 15:05:58 -0300

    test: add test para Currency composable

---
+++ tests/unit/useCurrency.spec.js
@@ -0,0 +1,25 @@
+import useCurrency from "@/composables/useCurrency"
+
+describe('UseCurrency - ', () => {
+
+    const amountMock = 100.000
+    const { formatCryptoValueIn } = useCurrency()
+
+    it('formatCryptoValueIn - should return the value with the default two digits', () => {
+        
+        const expectedValue = formatCryptoValueIn('usd', amountMock)
+
+        expect(expectedValue).toBe('$ 100.00')
+
+    })
+
+    it('formatCryptoValueIn - should return the value with five digits', () => {
+        
+        const expectedValue = formatCryptoValueIn('usd', amountMock, 5)
+
+        expect(expectedValue).toBe('$ 100.00000')
+
+    })
+
+
+})
