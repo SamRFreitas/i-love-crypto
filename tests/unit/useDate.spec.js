99644da7161330d34750d244a4a751ef03857fec
samrfreitas <samuelrfreitassi@gmail.com>
2024-06-01 15:07:31 -0300

    test: add test for date composable

---
+++ tests/unit/useDate.spec.js
@@ -0,0 +1,33 @@
+import useDate from "@/composables/useDate"
+
+describe('UseDate - ', () => {
+
+    const dateString = '2024-01-01T03:00:00Z'
+    const datetimeMock = new Date(dateString.toString())
+
+    it('range - should have the correct time interval of 1 hour in timestamps', () => {
+        
+        const { range } = useDate(datetimeMock)
+
+        expect(range.from).toBe(1704078000)
+
+        expect(range.to).toBe(1704081600)
+
+
+    })
+
+
+    it('formatDate - should return the correct format in month/day/year, hour:minutes', () => {
+        
+        const { formatDate } = useDate(datetimeMock)
+
+        const formattedDate = formatDate(datetimeMock)
+        const expectedDate = '01/01/2024, 00:001'
+        
+
+        expect(formattedDate).toBe(expectedDate)
+
+
+    })
+
+})
